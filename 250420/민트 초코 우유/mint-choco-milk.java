490531898d738aab9ef8725fa0b15e67f9b4d3f0
Jun_AK <51697727+0710june@users.noreply.github.com>
2025-04-20 23:16:43 +0900

    Title: 민트 초코 우유, Time: 329ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250420/민트 초코 우유/mint-choco-milk.java
+++ 250420/민트 초코 우유/mint-choco-milk.java
@@ -1,221 +1,308 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
-// 민트 초코 우유
 public class Main {
+    static int N,T;
+    static Member[][] memberMap;
+    static boolean[][] isGrouped;
+    static boolean[][] isDamaged;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer stk;
+    static int [] dx = {0,0,-1,1};
+    static int [] dy = {-1,1,0,0};
 
-    static int N, T;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    static Student[][] students;
-    static boolean[][] visited;
-    static StringBuilder sb = new StringBuilder();
-    static ArrayList<Student>[] leaders = new ArrayList[3];
+    // 민트 초코 우유
+    public static void main(String[] args) throws IOException {
+        init();
+        for (int i = 0; i < T; i++) {
+            morning();
+            List<Group> groups = lunch();
+            evening(groups);
+            resetDay();
+            afterEvening();
+        }
+    }
 
-    static class Student implements Comparable<Student>{
-        int x, y;
-        boolean mint, choco, milk;
-        int faith, earnest, dir;
-        boolean isAttacked;
+    private static void afterEvening() {
+        int [] result = new int[8];
 
-        Student(int x, int y, boolean mint, boolean choco, boolean milk) {
-            this.x = x;
-            this.y = y;
-            this.mint = mint;
-            this.choco = choco;
-            this.milk = milk;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                int type = findWho(memberMap[i][j]);
+                result[type] += memberMap[i][j].value;
+            }
         }
 
-        public void setFaith(int faith) {
-            this.faith = faith;
+        for (int i = 1; i <= 7 ; i++) {
+            System.out.print(result[i]+" ");
+        }
+        System.out.println();
+    }
+
+    private static void resetDay() {
+        isDamaged = new boolean[N+1][N+1];
+        isGrouped = new boolean[N+1][N+1];
+    }
+
+    private static void init() throws IOException {
+        stk = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(stk.nextToken());
+        T = Integer.parseInt(stk.nextToken());
+        isGrouped = new boolean[N+1][N+1];
+        isDamaged = new boolean[N+1][N+1];
+        memberMap = new Member[N+1][N+1];
+        for (int i = 1; i <= N; i++) {
+            String line = br.readLine();
+            for (int j = 1; j <= N; j++) {
+                memberMap[i][j] = new Member(i, j);
+                memberMap[i][j].set.add(line.charAt(j-1));
+            }
         }
 
-        public boolean isEqualFood(Student student) {
-            return this.mint == student.mint && this.choco == student.choco && this.milk == student.milk;
+        for (int i = 1; i <= N; i++) {
+            stk = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                memberMap[i][j].value = Integer.parseInt(stk.nextToken());
+            }
+
+        }
+    }
+
+    private static void morning(){
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                memberMap[i][j].value++;
+            }
+        }
+    }
+
+    private static List<Group> lunch(){
+        List<Group> groups = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if(!isGrouped[i][j]){
+                    groups.add(findGroup(i,j));
+                }
+            }
+        }
+        for (Group group : groups) {
+            int type = findWho(group.getLeader());
+            Member leader = group.getLeader();
+            group.setTypeCode(type);
+            group.setTypeCodeSize(memberMap[leader.y][leader.x].set.size());
+            for(Member member : group.members){
+                if(leader.equals(member))
+                    continue;
+                memberMap[member.y][member.x].value--;
+                memberMap[leader.y][leader.x].value++;
+            }
+        }
+        return groups;
+    }
+
+    private static Group findGroup(int startY, int startX) {
+        Group curGroup = new Group();
+        Member startMember = memberMap[startY][startX];
+        curGroup.members.add(startMember);
+        isGrouped[startY][startX] = true;
+        Deque<Member> deque = new ArrayDeque<>();
+        deque.add(startMember);
+
+        while(!deque.isEmpty()){
+            Member curMember = deque.poll();
+            for (int i = 0; i < 4; i++) {
+                int nxtY = curMember.y + dy[i];
+                int nxtX = curMember.x + dx[i];
+
+                if(1 > nxtY || N < nxtY || 1 > nxtX || N < nxtX)
+                    continue;
+
+                if(isGrouped[nxtY][nxtX])
+                    continue;
+
+                if(!checkSameSet(curMember.y,curMember.x,nxtY,nxtX))
+                    continue;
+
+                Member newMember = memberMap[nxtY][nxtX];
+                isGrouped[nxtY][nxtX] = true;
+                curGroup.members.add(newMember);
+                deque.add(newMember);
+            }
+        }
+        return curGroup;
+    }
+    private static int findWho(Member member){
+
+        // 민트초코우유 민트초코 민트우유 초코우유 우유 초코 민트
+        // TCM        TC      TM     CM     M    C   T
+        //  1         2        3     4     5     6    7
+        if(memberMap[member.y][member.x].set.size() == 3){
+            //민트초코우유
+            return 1;
+        }
+        else{
+            if(memberMap[member.y][member.x].set.size() == 2){
+                if(memberMap[member.y][member.x].set.contains('T') && memberMap[member.y][member.x].set.contains('C')){
+                    return 2;
+                }
+                if(memberMap[member.y][member.x].set.contains('M') && memberMap[member.y][member.x].set.contains('T')){
+                    return 3;
+                }
+                if(memberMap[member.y][member.x].set.contains('C') && memberMap[member.y][member.x].set.contains('M')){
+                    return 4;
+                }
+            }else{
+                if(memberMap[member.y][member.x].set.contains('M')){
+                    return 5;
+                }
+                if(memberMap[member.y][member.x].set.contains('C')){
+                    return 6;
+                }
+                if(memberMap[member.y][member.x].set.contains('T')){
+                    return 7;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static boolean checkSameSet(int y, int x, int nxtY, int nxtX) {
+        if(memberMap[y][x].set.size() != memberMap[nxtY][nxtX].set.size())
+            return false;
+        return memberMap[y][x].set.containsAll(memberMap[nxtY][nxtX].set) && memberMap[nxtY][nxtX].set.containsAll(memberMap[y][x].set);
+    }
+
+    private static void evening(List<Group> groups){
+        groups.sort((a, b) -> {
+
+            if (a.typeCodeSize != b.typeCodeSize)
+                return Integer.compare(a.typeCodeSize, b.typeCodeSize);
+
+            int aVal = memberMap[a.getLeader().y][a.getLeader().x].value;
+            int bVal = memberMap[b.getLeader().y][b.getLeader().x].value;
+            if (aVal != bVal)
+                return Integer.compare(bVal, aVal);
+
+            if (a.getLeader().y != b.getLeader().y)
+                return Integer.compare(a.getLeader().y, b.getLeader().y);
+
+            return Integer.compare(a.getLeader().x, b.getLeader().x);
+        });
+
+        // 전파
+        for (Group group : groups) {
+            Member leader = group.getLeader();
+            if(isDamaged[leader.y][leader.x])
+                continue;
+            int power = leader.getPower();
+            int dir = (power+1) % 4;
+
+            int nxtY = leader.y;
+            int nxtX = leader.x;
+            int curType = findWho(leader);
+            do {
+                nxtY += dy[dir];
+                nxtX += dx[dir];
+
+                if (1 > nxtY || N < nxtY || 1 > nxtX || N < nxtX)
+                    break;
+
+                Member target = memberMap[nxtY][nxtX];
+                int nxtType = findWho(target);
+
+                if(curType == nxtType)
+                    continue;
+
+                if(power > memberMap[target.y][target.x].value){
+                    power -= (memberMap[target.y][target.x].value + 1);
+                    sameSet(leader,target);
+                    memberMap[target.y][target.x].value++;
+                    isDamaged[target.y][target.x] = true;
+                }else{
+                    addSet(leader,target);
+                    memberMap[target.y][target.x].value += power;
+                    isDamaged[target.y][target.x] = true;
+                    power = 0;
+                }
+                if(power == 0)
+                    break;
+
+            }while (true);
+
         }
 
-        public int getFoodIdx() {
-            if(mint && !choco && !milk) return 0;
-            else if(!mint && choco && !milk) return 1;
-            else if(!mint && !choco && milk) return 2;
-            else if(!mint && choco && milk) return 3;
-            else if(mint && !choco && milk) return 4;
-            else if(mint && choco && !milk) return 5;
-            else if(mint && choco && milk) return 6;
-            else return 7;
+    }
+
+    private static void addSet(Member leader, Member target) {
+        memberMap[target.y][target.x].set.addAll(memberMap[leader.y][leader.x].set);
+    }
+
+    private static void sameSet(Member leader, Member target) {
+        memberMap[target.y][target.x].set.clear();
+        memberMap[target.y][target.x].set.addAll(memberMap[leader.y][leader.x].set);
+
+    }
+
+    static class Group implements Comparable<Group> {
+        List<Member> members = new ArrayList<>();
+        int typeCode; // 조합 코드: 1~7
+        int typeCodeSize;
+        public void setTypeCodeSize(int typeCodeSize) {
+            this.typeCodeSize = typeCodeSize;
+        }
+        public void add(Member m) {
+            members.add(m);
         }
 
-        public void setEarnest() {
-            this.dir = faith % 4;
-            this.earnest = faith - 1;
-            this.faith = 1;
+        public void setTypeCode(int typeCode) {
+            this.typeCode = typeCode;
+        }
+        public Member getLeader() {
+            return Collections.min(members, Comparator
+                    .comparingInt((Member m) -> -m.value)                // 신앙심 높은 순
+                    .thenComparingInt(m -> m.y)                          // 행 번호 작은 순
+                    .thenComparingInt(m -> m.x)                          // 열 번호 작은 순
+            );
         }
 
-        public void strongSpread(Student leader) {
-            this.mint = leader.mint;
-            this.choco = leader.choco;
-            this.milk = leader.milk;
-            this.isAttacked = true;
-        }
-
-        public void weakSpread(Student leader) {
-            if(leader.mint) this.mint = true;
-            if(leader.choco) this.choco = true;
-            if(leader.milk) this.milk = true;
-            this.isAttacked = true;
+        public int getHoly(){
+            int result = 0;
+            for(Member member : members){
+                result += memberMap[member.y][member.x].value;
+            }
+            return result;
         }
 
         @Override
-        public int compareTo(Student o) {
-            if(this.faith != o.faith) return Integer.compare(o.faith, this.faith);
-            if(this.x != o.x) return Integer.compare(this.x, o.x);
-            return Integer.compare(this.y, o.y);
+        public int compareTo(Group o) {
+            return this.getLeader().compareTo(o.getLeader());
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    static class Member implements Comparable<Member> {
+        int x, y, value;
+        Set<Character> set = new HashSet<>();
 
-        // 0. 초기화
-        init();
-
-        for(int t=0; t<T; t++) {
-            for(int i=0; i<3; i++) {
-                leaders[i] = new ArrayList<>();
-            }
-
-            // 1. 대표자 선정
-            morningAndLunch();
-
-            // 2. 전파
-            dinner();
+        public Member(int y, int x) {
+            this.y = y;
+            this.x = x;
         }
-
-        System.out.print(sb.toString());
-    }
-
-    private static void init() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        T = Integer.parseInt(st.nextToken());
-        students = new Student[N][N];
-        for(int i=0; i<N; i++) {
-            String str = br.readLine();
-            for(int j=0; j<N; j++) {
-                char c = str.charAt(j);
-                if(c == 'T') students[i][j] = new Student(i, j, true, false, false);
-                else if(c == 'C') students[i][j] = new Student(i, j, false, true, false);
-                else students[i][j] = new Student(i, j, false, false, true);
-            }
+        public int getPower(){
+            int power = memberMap[y][x].value - 1;
+            memberMap[y][x].value = 1;
+            return power;
         }
+        @Override
+        public int compareTo(Member o) {
+            int thisValue = memberMap[this.y][this.x].value;
+            int otherValue = memberMap[o.y][o.x].value;
 
-        for(int i=0; i<N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<N; j++) {
-                int faith = Integer.parseInt(st.nextToken());
-                students[i][j].setFaith(faith);
-            }
+            if (thisValue != otherValue) return Integer.compare(otherValue,thisValue);
+            if (this.y != o.y) return Integer.compare(this.y, o.y);
+            return Integer.compare(this.x, o.x);
         }
     }
 
-    private static void morningAndLunch() {
-        visited = new boolean[N][N];
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                if(visited[i][j]) continue;
-                findLeader(i, j);
-            }
-        }
-    }
-
-    private static void findLeader(int x, int y) {
-        ArrayList<Student> group = new ArrayList<>();
-        group.add(students[x][y]);
-        ArrayDeque<int[]> q = new ArrayDeque<>();
-        q.add(new int[] {x, y});
-        visited[x][y] = true;
-
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-
-            for(int i=0; i<4; i++) {
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-
-                if(!inRange(nx, ny) || visited[nx][ny]) continue;
-                if(!students[nx][ny].isEqualFood(students[x][y])) continue;
-                group.add(students[nx][ny]);
-                q.add(new int[] {nx, ny});
-                visited[nx][ny] = true;
-            }
-        }
-
-        Collections.sort(group);
-        Student leader = group.get(0);
-        leader.faith += group.size();
-
-        leaders[leader.getFoodIdx() / 3].add(leader);
-    }
-
-    private static void dinner() {
-        for(int i=0; i<3; i++) {
-            Collections.sort(leaders[i]);
-        }
-
-        for(ArrayList<Student> group : leaders) {
-            if(group.isEmpty()) continue;
-            for(Student leader : group) {
-                if(leader.isAttacked) continue;
-                leader.setEarnest();
-                spread(leader);
-            }
-        }
-
-        int[] cnt = new int[7];
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                cnt[students[i][j].getFoodIdx()] += students[i][j].faith;
-                if(students[i][j].isAttacked) students[i][j].isAttacked = false;
-            }
-        }
-
-        for(int i=6; i>=0; i--) {
-            sb.append(cnt[i]).append(" ");
-        }
-        sb.append("\n");
-    }
-
-    private static void spread(Student leader) {
-        int x = leader.x;
-        int y = leader.y;
-        while(leader.earnest > 0) {
-            x += dx[leader.dir];
-            y += dy[leader.dir];
-
-            if(!inRange(x, y)) {
-                leader.earnest = 0;
-                break;
-            }
-            if(leader.isEqualFood(students[x][y])) continue;
-
-            // 강한 전파
-            if(leader.earnest > students[x][y].faith) {
-                students[x][y].faith += 1;
-                leader.earnest -= students[x][y].faith;
-                students[x][y].strongSpread(leader);
-            }
-            // 약한 전파
-            else{
-                students[x][y].faith += leader.earnest;
-                leader.earnest = 0;
-                students[x][y].weakSpread(leader);
-            }
-
-            if(leader.earnest == 0) break;
-        }
-    }
-
-    private static boolean inRange(int x, int y) {
-        return x >= 0 && y >=0 && x < N && y < N;
-    }
 }
